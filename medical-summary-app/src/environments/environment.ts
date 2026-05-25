79d15592705b2bb7be47865e7ad7b6b3fdaab247
alex.cirlugea <alexcirlugea2@gmail.com>
2026-05-25 14:33:20 +0300

    chore(config): update npm dependencies and environment variables

---
+++ medical-summary-app/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDvPDL-jDOW12uxO5jdAts2Ft99HOboOuE",
+    authDomain: "medical-summary-app.firebaseapp.com",
+    projectId: "medical-summary-app",
+    storageBucket: "medical-summary-app.firebasestorage.app",
+    messagingSenderId: "175009978110",
+    appId: "1:175009978110:web:f86bf1c1ff7c4d92ac37af"
+  }
+};
